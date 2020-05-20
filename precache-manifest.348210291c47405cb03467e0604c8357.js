5c3c9b77da1c62f16823f6332ff96b48a7051a4c
sourab <Sourabkapoor@yahoo,com>
2020-05-20 16:54:54 +0530

    Updates

---
+++ precache-manifest.348210291c47405cb03467e0604c8357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6a3585ed2d8172460b1719a1ae98eb",
+    "url": "/image-recog/index.html"
+  },
+  {
+    "revision": "e3c6dd8a5036105df030",
+    "url": "/image-recog/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d7e0aeee556ba7a197c9",
+    "url": "/image-recog/static/css/main.6bd4e77c.chunk.css"
+  },
+  {
+    "revision": "e3c6dd8a5036105df030",
+    "url": "/image-recog/static/js/2.c6b7c6d4.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/image-recog/static/js/2.c6b7c6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e0aeee556ba7a197c9",
+    "url": "/image-recog/static/js/main.354f0b42.chunk.js"
+  },
+  {
+    "revision": "18d9b3f972b9241e5aef",
+    "url": "/image-recog/static/js/runtime-main.55039385.js"
+  }
+]);
